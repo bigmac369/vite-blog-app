1ff453701d432f58e6d9c0cc589e84caf707848b
Eric Chua <81133922+bigmac369@users.noreply.github.com>
2025-05-29 04:40:47 +0800

    add comment section template to SingleBlog component

--- src/components/UserProfilePost.tsx
+++ src/components/UserProfilePost.tsx
@@ -1,13 +1,15 @@
-import { useNavigate } from "react-router";
+import { useNavigate, Link } from "react-router";
 
 const UserProfilePost = ({ post, onDelete }) => {
-
   const navigate = useNavigate();
 
   const { title, summary, createdAt, _id } = post;
   return (
     <div className="postContainer">
-      <div className="post-item border border-[#eee] p-5 rounded-sm">
+      <Link
+        to={`/post/${post._id}`}
+        className="post-item border border-[#eee] p-5 rounded-sm block"
+      >
         <div className="post-header flex justify-between mb-2">
           <div>
             <h3 className="post-title font-bold text-xl">{title}</h3>
@@ -18,17 +20,22 @@ const UserProfilePost = ({ post, onDelete }) => {
           <div className="post-action flex justify-center items-center gap-3">
             <button
               onClick={(e) => {
+                e.preventDefault(); // prevent default link behavior
                 e.stopPropagation(); // stop click from reaching Link
                 // handle edit logic here
                 navigate(`/edit-post/${_id}`);
               }}
-              className="bg-[#3498DB] text-white px-2 rounded-sm cursor-pointer"
+              className="bg-[#3498DB] hover:bg-blue-600 transition-colors text-white px-2 rounded-sm cursor-pointer"
             >
               Edit
             </button>
             <button
-              onClick={() => onDelete(_id)}
-              className="bg-[#E74C3C] text-white px-2 rounded-sm cursor-pointer"
+              onClick={(e) => {
+                e.preventDefault(); // prevent default link behavior
+                e.stopPropagation(); // stop click from reaching Link
+                onDelete(_id);
+              }}
+              className="bg-[#E74C3C] hover:bg-red-600 transition-colors text-white px-2 rounded-sm cursor-pointer"
             >
               Delete
             </button>
@@ -37,7 +44,7 @@ const UserProfilePost = ({ post, onDelete }) => {
         <div className="post-excerpt bg-[#F8F9FA] p-3.5 border-l-4 border-l-[#3498db]">
           {summary}
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- src/pages/SingleBlog.tsx
+++ src/pages/SingleBlog.tsx
@@ -24,12 +24,65 @@ const SingleBlog = () => {
   }, [id]);
 
   return (
-    <div className="max-w-3xl mx-auto p-6">
-      <h1 className="text-3xl font-bold mb-2">{post?.title}</h1>
-      <p className="text-gray-500 mb-6 italic">{post?.summary}</p>
-      <hr className="mb-6" />
-      <div className="prose prose-lg prose-slate">
-        <p>{post?.content}</p>
+    <div className="page-container max-w-3xl mx-auto p-6 min-h-screen">
+      {/* post container */}
+      <div className="post-container mb-8">
+        <div className="title-summary-container flex">
+          <div>
+            <h1 className="text-3xl font-bold mb-2">{post?.title}</h1>
+            <p className="text-gray-500 mb-6 italic">{post?.summary}</p>
+          </div>
+          <div className="flex items-start gap-4 ml-auto">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded text-sm transition-colors cursor-pointer">
+              Edit
+            </button>
+            <button className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition-colors cursor-pointer">
+              Delete
+            </button>
+          </div>
+        </div>
+
+        <hr className="mb-6" />
+        <div className="prose prose-lg prose-slate">
+          <p>{post?.content}</p>
+        </div>
+      </div>
+
+      {/* comments section */}
+      <div className="comments-section">
+        <h2 className="text-2xl font-medium mb-4">Comments</h2>
+        <div className="mb-8">
+          <div className="mb-4">
+            <textarea
+              className="border border-gray-300 rounded-lg min-h-[100px] p-3 w-full"
+              name=""
+              id=""
+            ></textarea>
+          </div>
+          <button className="bg-blue-500 hover:bg-blue-600 px-4 py-2 text-white transition-colors rounded cursor-pointer">
+            Add Comment
+          </button>
+        </div>
+        {/* Example Comment */}
+        <div className="comment-list-container">
+          <div className="comment-item bg-[#F9FAFB] p-2 rounded">
+            <div className="comment-content flex justify-between mb-2">
+              <div className="author-date-div">
+                <p className="font-medium">Author name</p>
+                <p className="text-sm text-gray-500">Date</p>
+              </div>
+              <div className="edit_delete-div flex gap-2 items-start">
+                <button className="text-blue-700 cursor-pointer">Edit</button>
+                <button className="text-red-700 cursor-pointer">Delete</button>
+              </div>
+            </div>
+            <p>
+              Great article! The examples really helped me understand hooks
+              better. I've been struggling with useEffect dependencies, and your
+              explanation cleared things up.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
